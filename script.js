3a696416005658258b76ff70b3635dda016831bd
Viktoriya Savaryn <37408680+plyus1313@users.noreply.github.com>
2018-05-17 10:59:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var _ = require('lodash');
+var array = [1,2,3,4,5,6];
+
+console.log('answer:', _.without(array, 3));
+
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+    body.style.background =
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+
+css.textContent = body.style.background + ";";
+}
+
+setGradient();
+
+ function getRandomColor() {
+//     var letters = '0123456789ABCDEF';
+//     var color = '#';
+//     for (var i = 0; i < 6; i++) {
+//       color += letters[Math.round(Math.random() * 16)];
+//
+color1.value= getRandomNumber();
+color2.value= getRandomNumber() ;
+}
+//     return color;
+//   }
+//
+// function setRandomColor() {
+//     $(".color1").val(getRandomColor());
+//     $(".color2").val(getRandomColor());
+//     setGradient();
+  // }
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
